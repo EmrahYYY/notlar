2db6c13569b6946e043006486953638d959db693
EmrahYYY <emrah.yavuz@student.intecbrussel.be>
2021-10-18 15:40:37 +0200

    Initial commit

---
+++ src/notlarr.java
@@ -0,0 +1,22 @@
+public class notlarr {
+}
+
+shift+f6=metni deistir
+        kortom=kisacasi
+        streven=gayret etmek
+        implementatie=uygulama(eng)
+        declaratie=beyan etmek,duyurmak(eng)
+        definitie=tanim
+        curlybracket,accolades,=koseli parantez
+        subt-ract=cikar-ma(eng)
+        even-oneven getalen=cift-tek sayilar
+        calculate=hasaplama,berekenen(eng)
+        invalid=gecersiz(eng)
+        invalide=gecersiz(nl)
+        magnifique=muhtesem(fr,nl)
+        constructor=yapici-yaratan(eng)
+        construct=insa etmek(eng)
+        construeren=insa etmek(nl)
+        side=kant,taraf(eng)
+        useful=handig(eng)
+        derive,derivative=turetmek,turevi(eng)
